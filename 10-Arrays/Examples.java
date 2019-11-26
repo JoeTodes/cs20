cbbe8c383b07a18bfcea34b4d942ad7335dd63f3
JoeTodes <jtwilson.home@gmail.com>
2019-11-26 12:04:39 -0700

    array examples

--- 10-Arrays/Examples.java
+++ 10-Arrays/Examples.java
@@ -4,11 +4,28 @@
 public class Examples {
 
     public static void main(String[] args) {
-        String[] animals = { "cat", "dog", "bird", "otter", "pig" };
-
-        for (int i = 0; i < animals.length; i++) {
-            System.out.println(animals[i]);
+        // create an array of ints
+        int[] myArray = { 2, 6, 15, -3, 12 };
+        // print each element of the array on a new line
+        for (int i = 0; i < myArray.length; i++) {
+            // System.out.println(myArray[i]);
         }
 
+        // find the sum of the array
+        int total = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            total = total + myArray[i];
+        }
+        System.out.println("total is: " + total);
+
+        // find the highest number in the array
+        int max = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] > myArray[max]) {
+                max = i;
+            }
+        }
+        System.out.println("the maximum value is in position " + max);
+        System.out.println("the maximum value is " + myArray[max]);
     }
 }
